d2a08252e7123f4cc84c9d9fba4dc8eb5b16e077
stoffee <chris@roadkill.org>
2023-02-08 20:25:21 -0800

    added eks-hvn-only-deploy example

--- examples/full-deploy/variables.tf
+++ examples/full-deploy/variables.tf
@@ -1,38 +1,44 @@
+variable "deploy_eks_cluster" {
+  type        = string
+  description = "Choose if you want an eks cluster to be provisioned"
+  default     = true
+}
+
 variable "cluster_id" {
   type        = string
-  description = "The name of your HCP Vault cluster"
-  default     = "hcp-vault"
+  description = "The name of your EKS cluster"
+  default     = "hcp-eks"
+}
+
+variable "vpc_region" {
+  type        = string
+  description = "The AWS region to create resources in"
+  default     = "us-west-2"
 }
 
 variable "eks_instance_types" {
-  type        = list
+  type        = list(any)
   description = "The node size of your EKS cluster"
   #default     = ["t3a.medium"]
-  default     = ["t2.small"]
+  default = ["t2.small"]
 }
 
-variable "hcp_vault_cluster_id" {
+variable "deploy_hvn" {
   type        = string
-  description = "The name of your HCP Vault cluster"
-  default     = "hcp-vault"
-}
-
-variable "vault_internal_addr" {
-  type        = string
-  description = "The internal url of your existing HCP vault cluster"
-  default     = "https://MyVault.private.vault.f4cfade2-df28-47f2-a365-56gb2a62d8c5.aws.hashicorp.cloud:8200"
-}
-
-variable "hvn_cidr_block" {
-  type        = string
-  description = "The CIDR range to create the HCP HVN with if we are creating a new one"
-  default     = "172.25.32.0/20"
+  description = "Choose to deploy HCP HVP or use an existing one"
+  default     = false
 }
 
 variable "hvn_id" {
   type        = string
-  description = "The name of your HCP HVN"
+  description = "The name of your existing HCP HVN"
   default     = "PUT-YOUR-HVN-NAME-HERE"
+}
+
+variable "hvn_cidr_block" {
+  type        = string
+  description = "The CIDR range to create the HCP HVN with"
+  default     = "172.25.32.0/20"
 }
 
 variable "hvn_region" {
@@ -47,26 +53,27 @@ variable "deploy_vault_cluster" {
   default     = false
 }
 
-variable "deploy_hvn" {
+variable "make_vault_public" {
   type        = string
-  description = "Choose to deploy HCP HVP or use an existing one"
+  description = "Choose if you want your Vault cluster to have a public address"
   default     = false
 }
 
-variable "deploy_eks_cluster" {
+variable "hcp_vault_cluster_id" {
   type        = string
-  description = "Choose if you want an eks cluster to be provisioned"
-  default     = true
+  description = "The name of your HCP Vault cluster"
+  default     = "hcp-vault"
 }
 
-variable "tier" {
+variable "vault_internal_addr" {
   type        = string
-  description = "The HCP Consul tier to use when creating a Consul cluster"
+  description = "The internal url of your HCP vault cluster"
+  default     = "https://MyVault.private.vault.f4cfade2-df28-47f2-a365-56gb2a62d8c5.aws.hashicorp.cloud:8200"
+}
+
+
+variable "vault_tier" {
+  type        = string
+  description = "The HCP Vault tier to use when creating a Vault cluster"
   default     = "development"
-}
-
-variable "vpc_region" {
-  type        = string
-  description = "The AWS region to create resources in"
-  default     = "us-west-2"
 }
