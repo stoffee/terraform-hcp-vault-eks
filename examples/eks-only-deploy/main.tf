7fd96b1b7b548b0fdfb5d058120d2588f2043065
stoffee <chris@roadkill.org>
2023-03-02 08:59:46 -1000

    updated examples

--- examples/eks-only-deploy/main.tf
+++ examples/eks-only-deploy/main.tf
@@ -1,7 +1,7 @@
 module "hcp-eks" {
-  source = "../../"
-  #source               = "stoffee/vault-eks/hcp"
-  #version              = "~> 0.0.7"
+  #source = "../../"
+  source               = "stoffee/vault-eks/hcp"
+  version              = "~> 0.0.7"
   cluster_id           = var.cluster_id
   deploy_hvn           = var.deploy_hvn
   hvn_id               = var.hvn_id
